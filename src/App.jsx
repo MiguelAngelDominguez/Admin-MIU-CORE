ef12a89a6196ab3a2916e2548cfb0cd40b5f3e2a
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-06-26 16:30:43 -0500

    Se comenzo con el aprendizaje de MUI-core junto con react, ya se implemento algunas etiquetas en src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,255 @@
+import * as React from 'react';
+import { Autocomplete, Box, Button, Chip, Stack, TextField } from "@mui/material";
+import { createFilterOptions } from '@mui/material/Autocomplete';
+
+const filter = createFilterOptions();
+
+export default function App() {
+    const options = top101Films.map((option) => {
+        const firstLetter = option.title[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
+    });
+
+    const [value, setValue] = React.useState(null);
+
+    return (
+        <>
+            <h1>Text</h1>
+
+            <h2>Button</h2>
+            <Button variant="contained">Mi Primer Boton</Button>
+
+            <h2>Select</h2>
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+            />
+            <h2>Select-Country</h2>
+            <Autocomplete
+                id="country-select-demo"
+                sx={{ width: 300 }}
+                options={countries}
+                autoHighlight
+                getOptionLabel={(option) => option.label}
+                renderOption={(props, option) => (
+                    <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+                        <img
+                            loading="lazy"
+                            width="20"
+                            src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                            srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                            alt=""
+                        />
+                        {option.label} ({option.code}) +{option.phone}
+                    </Box>
+                )}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Choose a country"
+                        inputProps={{
+                            ...params.inputProps,
+                            autoComplete: 'new-password', // disable autocomplete and autofill
+                        }}
+                    />
+                )}
+            />
+            <h2>Autocomplate-Input</h2>
+            <Autocomplete
+                value={value}
+                onChange={(event, newValue) => {
+                    if (typeof newValue === 'string') {
+                        setValue({
+                            title: newValue,
+                        });
+                    } else if (newValue && newValue.inputValue) {
+                        // Create a new value from the user input
+                        setValue({
+                            title: newValue.inputValue,
+                        });
+                    } else {
+                        setValue(newValue);
+                    }
+                }}
+                filterOptions={(options, params) => {
+                    const filtered = filter(options, params);
+
+                    const { inputValue } = params;
+                    // Suggest the creation of a new value
+                    const isExisting = options.some((option) => inputValue === option.title);
+                    if (inputValue !== '' && !isExisting) {
+                        filtered.push({
+                            inputValue,
+                            title: `Add "${inputValue}"`,
+                        });
+                    }
+
+                    return filtered;
+                }}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                id="free-solo-with-text-demo"
+                options={top101Films}
+                getOptionLabel={(option) => {
+                    // Value selected with enter, right from the input
+                    if (typeof option === 'string') {
+                        return option;
+                    }
+                    // Add "xxx" option created dynamically
+                    if (option.inputValue) {
+                        return option.inputValue;
+                    }
+                    // Regular option
+                    return option.title;
+                }}
+                renderOption={(props, option) => <li {...props}>{option.title}</li>}
+                sx={{ width: 300 }}
+                freeSolo
+                renderInput={(params) => (
+                    <TextField {...params} label="Free solo with text demo" />
+                )}
+            />
+
+            <h2>Autocomplate-Grouped</h2>
+            <Autocomplete
+                id="grouped-demo"
+                options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                groupBy={(option) => option.firstLetter}
+                getOptionLabel={(option) => option.title}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="With categories" />}
+            />
+
+            <Stack spacing={3} sx={{ width: 500 }}>
+                <Autocomplete
+                    multiple
+                    id="tags-standard"
+                    options={top101Films}
+                    getOptionLabel={(option) => option.title}
+                    defaultValue={[top101Films[13]]}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="standard"
+                            label="Multiple values"
+                            placeholder="Favorites"
+                        />
+                    )}
+                />
+                {/* <Autocomplete
+                    multiple
+                    id="tags-outlined"
+                    options={top101Films}
+                    getOptionLabel={(option) => option.title}
+                    defaultValue={[top101Films[13]]}
+                    filterSelectedOptions
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="filterSelectedOptions"
+                            placeholder="Favorites"
+                        />
+                    )}
+                /> */}
+                {/* <Autocomplete
+                    multiple
+                    id="tags-filled"
+                    options={top101Films.map((option) => option.title)}
+                    defaultValue={[top101Films[13].title]}
+                    freeSolo
+                    renderTags={(value, getTagProps) =>
+                        value.map((option, index) => (
+                            <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                        ))
+                    }
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="filled"
+                            label="freeSolo"
+                            placeholder="Favorites"
+                        />
+                    )}
+                /> */}
+                <Autocomplete
+                    multiple
+                    id="tags-readOnly"
+                    options={top101Films.map((option) => option.title)}
+                    defaultValue={[top101Films[12].title, top101Films[13].title]}
+                    readOnly
+                    renderInput={(params) => (
+                        <TextField {...params} label="readOnly" placeholder="Favorites" />
+                    )}
+                />
+            </Stack>
+
+        </>
+    );
+}
+
+const top100Films = [
+    { label: 'The Silence of the Lambs', year: 1991 },
+    { label: "It's a Wonderful Life", year: 1946 },
+    { label: 'Life Is Beautiful', year: 1997 },
+    { label: 'The Usual Suspects', year: 1995 },
+    { label: 'Léon: The Professional', year: 1994 },
+    { label: 'Spirited Away', year: 2001 },
+    { label: 'Saving Private Ryan', year: 1998 },
+    { label: 'Once Upon a Time in the West', year: 1968 },
+    { label: 'American History X', year: 1998 },
+    { label: 'Interstellar', year: 2014 },
+    { label: 'Casablanca', year: 1942 },
+    { label: 'City Lights', year: 1931 },
+    { label: 'Psycho', year: 1960 },
+    { label: 'The Green Mile', year: 1999 },
+    { label: 'The Intouchables', year: 2011 },
+    { label: 'Modern Times', year: 1936 },
+    { label: 'Raiders of the Lost Ark', year: 1981 },
+    { label: 'Rear Window', year: 1954 },
+    { label: 'The Pianist', year: 2002 },
+    { label: 'The Departed', year: 2006 },
+    { label: 'Terminator 2: Judgment Day', year: 1991 },
+    { label: 'Back to the Future', year: 1985 },
+];
+
+const top101Films = [
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+];
+
+const countries = [
+    { code: 'AI', label: 'Anguilla', phone: '1-264' },
+    { code: 'AL', label: 'Albania', phone: '355' },
+    { code: 'AM', label: 'Armenia', phone: '374' },
+    { code: 'AO', label: 'Angola', phone: '244' },
+    { code: 'AQ', label: 'Antarctica', phone: '672' },
+    { code: 'AR', label: 'Argentina', phone: '54' },
+    { code: 'AS', label: 'American Samoa', phone: '1-684' },
+    { code: 'AT', label: 'Austria', phone: '43' },
+];
